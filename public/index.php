a37ec1a188dcbd4ffd0797ebf44362c125c02a4f
Justin Dietert <justin.dietert@gmail.com>
2015-03-23 19:03:54 -0500

    cleaned up code and moved Modernizr to base of body

--- public/index.php
+++ public/index.php
@@ -4,33 +4,28 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>JD | Justin Dietert</title>
-
     <!-- Normalize CSS -->
     <link rel="stylesheet" href="/css/normalize.css" />
-
     <!-- Foundation CSS -->
     <link rel="stylesheet" href="/css/foundation.min.css" />
-
     <!-- Custom Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600' rel='stylesheet' type='text/css' />
-
     <!-- FontAwesome icons-->
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />
-
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/css/custom.css" />
-
-    <!-- Modernizr.js -->
-    <script src="/js/vendor/modernizr.js"></script>
     </head>
-
     <body>
 
-
+        <!-- jQuery -->
         <script src="/js/vendor/jquery.js"></script>
+        <!-- Modernizr.js -->
+        <script src="/js/vendor/modernizr.js"></script>
+        <!-- Foundation JS -->
         <script src="/js/foundation.min.js"></script>
         <script>
             $(document).foundation();
+            // Custom Script Below //
         </script>
     </body>
 </html>
