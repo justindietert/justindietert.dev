f1c1ae62bafb7d4e1a93fe08a743314d7375310f
Justin Dietert <justin.dietert@gmail.com>
2015-04-03 17:58:40 -0500

    implementing site nav, starting point

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,18 @@
 
+/* Main Nav Toggle for Small Screens
+------------------------------------------------------- */
+$('a.menu').click(
+    function() {
+
+    $('nav[role="navigation"]').toggle();
+
+    if ($('a.menu').html() == '<i class="fa fa-bars"></i>') {
+        $('a.menu').html('<i class="fa fa-times"></i>');
+
+    } else {
+        $('a.menu').html('<i class="fa fa-bars"></i>');
+
+    }
+
+    return false;
+});
